7da9dc0b6fbed250764892b5feed73dfcaa43f3c
mario valdebenito <mario_valdebenito@hotmail.com>
2023-12-10 18:38:47 -0300

    mas cambios

--- src/Componentes/MyApi.jsx
+++ src/Componentes/MyApi.jsx
@@ -2,14 +2,16 @@ import { useState, useEffect } from "react";
 
 const MyApi = ({ feriados, setFeriados }) => {
   useEffect(() => {
- 
-      const data = fetch("https://api.victorsanmartin.com/feriados/en.json")
-        .then((data) => data.json())
-        .then((f) => f);
-        setFeriados{data};
-    );
+    fetch("https://api.victorsanmartin.com/feriados/en.json")
+      .then(data => data.json())
+      .then(f => setFeriados(f.data);
+    setFeriados(f.data));
   }, []);
 
-  return <div>MIAPI</div>;
-};
+  return( 
+    <div>
+      {feriados.map(f=><p>{`${f.title}-${new Date (f.date).getDate}`}</p>)}
+    </div>;
+  )
+  
 export default MyApi;
